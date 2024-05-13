e60e187d159223cedfbe6de645c808bb588f9baa
fiorittoev <fiorittoev@gmail.com>
2024-05-12 21:34:46 -0400

    Added panels

--- evansresume/src/Home.js
+++ evansresume/src/Home.js
@@ -7,12 +7,16 @@ import './Home.css';
 
 function Home() {
 
-    const parallax = useParallax({
-      opacity:[0,100],
-    });
+    //const parallax = useParallax({
+    //  rotate:[0,100],
+    //});
 
     return (
       <div className="Home">
+
+        <div className="Left-panel">
+        </div>
+
         <header className="Home-header">
 
           <img src={logo} className="Home-logo" alt="logo" />
@@ -21,17 +25,20 @@ function Home() {
             Evan Fioritto
           </h1>
           
-          <div ref={parallax.ref} className="Links">
+          <div className="Links">
               <a target="_blank" href="https://devpost.com/fiorittoev" rel="noreferrer"><img src={devpostLogo} className="Devpost-logo" alt="devpost"/></a>
               <a target="_blank" href="https://github.com/fiorittoev" rel="noreferrer"><img src={githubLogo} className="Github-logo" alt="github" /></a>
               <a target="_blank" href="https://www.linkedin.com/in/evanfioritto/" rel="noreferrer"><img src={linkedInLogo} className="Linkedin-logo" alt="linkedin"/></a>
           </div>
 
-          <h2  className="Desc">
+          <h2 className="Desc">
             Junior in CS and IT at Michigan State University
           </h2>
 
         </header>
+
+        <div className="Right-panel">
+        </div>
 
 
       </div>
